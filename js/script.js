cb420d8491094b430a0bda3c564aae992f84fa57
Jonas Torquatro <60902980+CipherCrow@users.noreply.github.com>
2021-04-13 18:29:06 -0300

    :up: Adicionando Jquery
    Com jquery, facilita na hora de fazer diversas modificações em javascript.
    Slider criado

--- js/script.js
+++ js/script.js
@@ -17,3 +17,52 @@ menuMobileAtivador.addEventListener('click',function(){
 	}
 	
 })
+
+	//slider
+$(function(){
+	var delay = 4000;
+	var curIndex = 0;
+	var amt;
+
+	initSlider();
+	autoPlay();
+
+	function initSlider(){
+		amt = $('.single-autor').length;
+		var sizeContainer = 100 * amt;
+		var sizeBoxSingle = 100 / amt;
+		$('.single-autor').css('width',sizeBoxSingle+'%');
+		$('.scroll-wrapper').css('width',sizeContainer+'%');
+	
+		for(var i=0; i< amt;i++){
+			if(i==0){
+				$('.slider-bullets').append('<span class="selected"></span>');
+			}else{
+				$('.slider-bullets').append('<span></span>');
+			}
+		}
+	};
+
+	function autoPlay(){
+		setInterval(function(){
+			curIndex++;
+			if(curIndex == amt){
+				curIndex = 0;
+			}
+			goNext(curIndex);
+		},delay);
+	}
+	function goNext(curIndex){
+		var offSetX = $('.single-autor').eq(curIndex).offset().left - $('.scroll-wrapper').offset().left;
+		$('.slider-bullets span').removeClass('selected');
+		$('.slider-bullets span').eq(curIndex).addClass('selected');
+		$('.scroll-equipe').stop().animate({
+			'scrollLeft':offSetX+'rem'
+		})
+	}
+	$(window).resize(function(){
+		$('.scroll-equipe').stop().animate({
+			'scrollLeft':0
+		})
+	})
+})
